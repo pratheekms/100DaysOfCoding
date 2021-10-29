9a695c415c5aa4b7a26a02f84f9054da7f2572bb
pratheek <27886811+pratheekms@users.noreply.github.com>
2021-10-29 19:32:15 +0530

    list

--- p_lists.py
+++ p_lists.py
@@ -8,3 +8,13 @@ print(new1[0][3])
 rlist=random.sample(range(10,50),10)
 print(rlist)
 
+newl=[x for x in range(10) if x%2==0]
+print(newl)
+
+slist=['a','b','c','A','B','C']
+slist.sort()
+print(slist)
+slist.sort(key=str.lower)
+print(slist)
+
+print('list')
