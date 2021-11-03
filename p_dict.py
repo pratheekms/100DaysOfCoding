6da9e0383afd32864f5194cc93ea646408f5d825
pratheekms <ms.pratheek1@gmail.com>
2021-11-03 20:05:24 +0530

    dict

--- p_dict.py
+++ p_dict.py
@@ -1,1 +1,24 @@
 #dict
+d1={
+    1:'one',
+    2:'two',
+    3:'three',
+    4:'TWO'
+}
+
+print(d1)
+
+for i in d1.values():
+    print(i)
+
+for i in d1.keys():
+    print(i)
+
+for i,j in d1.items():
+    print(i,j)
+
+print(d1[1])
+print(d1.get(4))
+
+d1.update({4:'four'})
+print(d1)
