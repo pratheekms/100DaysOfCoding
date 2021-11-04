0a0768d5b0de67008fb512f96e058676987b0448
pratheek <27886811+pratheekms@users.noreply.github.com>
2021-11-04 17:28:44 +0530

    table created

--- general_function.py
+++ general_function.py
@@ -1,30 +1,36 @@
-def add_func(num1: int, num2: int)-> int:
+def add_func(num1: int, num2: int) -> int:
     return num1+num2
 
-def sub_func(num1: int, num2: int)-> int:
+
+def sub_func(num1: int, num2: int) -> int:
     return num1-num2
 
-add_s='Sum of {} and {} is {}'
-sub_s='Difference between {} and {} is {}'
-print(add_s.format(4,5,add_func(4,5)))
-print(sub_s.format(10,5,sub_func(10,5)))
+
+add_s = 'Sum of {} and {} is {}'
+sub_s = 'Difference between {} and {} is {}'
+print(add_s.format(4, 5, add_func(4, 5)))
+print(sub_s.format(10, 5, sub_func(10, 5)))
 
 
-new_list=list(('pratheek',25,'banglore')) #list constructor
+new_list = list(('pratheek', 25, 'banglore'))  # list constructor
 print(new_list)
 
-def new_func2(name: str,age:int)->str:
-    return 'my name is {} and i\'m {} years old'.format(name,age)
 
-my_dict={'john':20,'mark':25,'jake':35,'amy':32,'rachel':23}
+def new_func2(name: str, age: int) -> str:
+    return 'my name is {} and i\'m {} years old'.format(name, age)
 
-for name,age in my_dict.items():
+
+my_dict = {'john': 20, 'mark': 25, 'jake': 35, 'amy': 32, 'rachel': 23}
+
+for name, age in my_dict.items():
     # print(name, age)
-    print(new_func2(name,age))
-#check for dict update
+    print(new_func2(name, age))
+# check for dict update
 
-def new_func3(place:str)->str:
+
+def new_func3(place: str) -> str:
     return place
+
 
 print(new_func3('banglore'))
 print(new_func3('newyork'))
