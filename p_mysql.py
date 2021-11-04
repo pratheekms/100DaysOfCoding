a931c3b3cef209df6918da9b54fe6f73a1fe2dc9
pratheek <27886811+pratheekms@users.noreply.github.com>
2021-11-04 20:08:55 +0530

    mysql complete

--- p_mysql.py
+++ p_mysql.py
@@ -30,11 +30,66 @@ mycursor = db.cursor()
 # mycursor.execute('show databases')
 
 
-#create table
+# create table
 # mycursor.execute("CREATE TABLE customers (name VARCHAR(255), address VARCHAR(255))")
 
 # print(x for x in mycursor.execute("SHOW TABLES"))
+# sql="DROP TABLE customrs"
 mycursor.execute("SHOW TABLES")
 
 for x in mycursor:
     print(x)
+
+# sql="ALTER TABLE customers ADD COLUMN id INT AUTO_INCREMENT PRIMARY KEY"
+
+# sql='INSERT INTO customers (name,address) VALUES ( %s,%s)'
+# val=('cena','canada')
+# mycursor.execute(sql,val)
+# db.commit()
+# print(mycursor.rowcount,"record inserted")
+
+# excetuce many, for multiple insert
+# sql = "INSERT INTO customers (name, address) VALUES (%s, %s)"
+# val = [
+#   ('Peter', 'Lowstreet 4'),
+#   ('Amy', 'Apple st 652'),
+#   ('Hannah', 'Mountain 21'),
+#   ('Michael', 'Valley 345'),
+#   ('Sandy', 'Ocean blvd 2'),
+#   ('Betty', 'Green Grass 1'),
+#   ('Richard', 'Sky st 331'),
+#   ('Susan', 'One way 98'),
+#   ('Vicky', 'Yellow Garden 2'),
+#   ('Ben', 'Park Lane 38'),
+#   ('William', 'Central st 954'),
+#   ('Chuck', 'Main Road 989'),
+#   ('Viola', 'Sideway 1633')
+# ]
+
+# mycursor.executemany(sql, val)
+
+# db.commit()
+
+# print(mycursor.rowcount, "was inserted.") 
+
+sql="select * from customers"
+mycursor.execute(sql)
+# myresult=mycursor.fetchall()
+myresult=mycursor.fetchone()
+for x in myresult:
+    print(x)
+
+# sql = "SELECT * FROM customers WHERE name =%s"
+# # adr = ("Yellow Garden 2", )
+# name=("john",)
+
+# mycursor.execute(sql,name)
+
+# res=mycursor.fetchall()
+# for x in res:
+#     print(x)
+
+# sql_update="UPDATE customers SET NAME=%s WHERE NAME=%s"
+# name=('john','JOHN')
+# mycursor.execute(sql_update,name)
+# db.commit()
