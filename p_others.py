30e601928e29f19a6e4b6d8b0bed5d336d684a61
pratheek <27886811+pratheekms@users.noreply.github.com>
2021-11-04 11:43:40 +0530

    other

---
+++ p_others.py
@@ -0,0 +1,25 @@
+from typing import Match, Optional, SupportsComplex
+
+
+# iterators
+# scope 
+# module 
+# dates 
+# math 
+# json
+# regex 
+# pip 
+# try..except 
+# user iput 
+# string formating 
+
+#iter....__iter__(), __next__()
+#create an iter object and use next to iter on them
+
+list1=[1,2,3,4,5,6,7]
+itl=iter(list1)
+print(next(itl))
+print(next(itl))
+print(next(itl))
+
+#python dates
