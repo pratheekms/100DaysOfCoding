e874f946607a82eda4d47693e7aec44844b58eff
pratheek <27886811+pratheekms@users.noreply.github.com>
2021-11-04 13:02:07 +0530

    basic end

--- p_others.py
+++ p_others.py
@@ -23,3 +23,15 @@ print(next(itl))
 print(next(itl))
 
 #python dates
+import datetime
+x=datetime.datetime.now()
+print(x,x.strftime('%Z'))
+print(x.year)
+
+#strftime
+y=datetime.datetime(2021,11,4)
+print(y)
+print(y.strftime('%C'))
+
+myorder='your order is {} {} with a price {:.2f}'
+print(myorder.format('burger','fries',23.3423))
