4b6d53e117040f14a46cf64ae9e1a489077b98e6
pratheek <27886811+pratheekms@users.noreply.github.com>
2021-11-05 23:24:07 +0530

    main created

--- teachers_staff_room/source/db.py
+++ teachers_staff_room/source/db.py
@@ -5,6 +5,17 @@ import dbconfig
 
 
 def create_conn(db_name:str)->mysql.connector:
+    """[summary]
+
+    Args:
+        db_name (str): [description]
+
+    Raises:
+        ValueError: [description]
+
+    Returns:
+        mysql.connector: [description]
+    """    
     if db_name != dbconfig.DATABASE_CONFIG['dbname']:
         raise ValueError("Couldn't not find DB with given name")
     conn=mysql.connector.connect(host=dbconfig.DATABASE_CONFIG['host'],
@@ -12,3 +23,12 @@ def create_conn(db_name:str)->mysql.connector:
                            password=dbconfig.DATABASE_CONFIG['password'],
                            db=dbconfig.DATABASE_CONFIG['dbname'])
     return conn
+
+def print_table_data(db_conn:mysql.connector)->None:
+    sql="select * from teachers"
+    mycursor=db_conn.cursor()
+    mycursor.execute(sql)
+    myresult=mycursor.fetchall()
+    print('Printing database records:->')
+    for x in myresult:
+        print(x)
