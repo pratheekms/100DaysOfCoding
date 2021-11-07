8a483ccb518c6f8b81355273c510c264826d3822
pratheek <27886811+pratheekms@users.noreply.github.com>
2021-11-07 22:13:35 +0530

    added doc string to db functions

--- teachers_staff_room/source/db.py
+++ teachers_staff_room/source/db.py
@@ -3,8 +3,7 @@ import mysql.connector
 import dbconfig
 
 
-
-def create_conn(db_name:str)->mysql.connector:
+def create_conn(db_name: str) -> mysql.connector:
     """[summary]
 
     Args:
@@ -15,97 +14,142 @@ def create_conn(db_name:str)->mysql.connector:
 
     Returns:
         mysql.connector: [description]
-    """    
+    """
     if db_name != dbconfig.DATABASE_CONFIG['dbname']:
         raise ValueError("Couldn't not find DB with given name")
-    conn=mysql.connector.connect(host=dbconfig.DATABASE_CONFIG['host'],
-                           user=dbconfig.DATABASE_CONFIG['user'],
-                           password=dbconfig.DATABASE_CONFIG['password'],
-                           db=dbconfig.DATABASE_CONFIG['dbname'])
+    conn = mysql.connector.connect(host=dbconfig.DATABASE_CONFIG['host'],
+                                   user=dbconfig.DATABASE_CONFIG['user'],
+                                   password=dbconfig.DATABASE_CONFIG['password'],
+                                   db=dbconfig.DATABASE_CONFIG['dbname'])
     return conn
 
-def print_table_data(db_conn:mysql.connector)->None:
-    sql="select * from teachers"
-    mycursor=db_conn.cursor()
+
+def print_table_data(db_conn: mysql.connector) -> None:
+    """[summary]
+
+    Args:
+        db_conn (mysql.connector): [description]
+    """    
+    sql = "select * from teachers"
+    mycursor = db_conn.cursor()
     mycursor.execute(sql)
-    myresult=mycursor.fetchall()
+    myresult = mycursor.fetchall()
     print('Printing database records:->')
     for x in myresult:
         print(x)
-    
 
 
 def insert_new_record(db_conn, u_input):
+    """[summary]
+
+    Args:
+        db_conn ([type]): [description]
+        u_input ([type]): [description]
+    """    
     sql = "INSERT INTO teachers (first_name, last_name,gender,title,qualification,primary_subject,\
 secondary_subject,contact_number,email_id) VALUES (%s, %s,%s, %s,%s, %s,%s, %s,%s)"
 
     mycursor = db_conn.cursor()
     mycursor.execute(sql, u_input)
     db_conn.commit()
-    print(mycursor.rowcount,"record inserted")
+    print(mycursor.rowcount, "record inserted")
     mycursor.execute("select LAST_INSERT_ID()")
-    t_id=mycursor.fetchone()
-    # print('teacher id is:',t_id[0])
+    t_id = mycursor.fetchone()
     return t_id[0]
 
 
-def view_teacher_details(db_conn,teacher_id):
-    if validate_teache_id(db_conn,teacher_id):
-        t_id=(teacher_id,)
-        sql="select * from teachers where id=%s"
-        
-        mycursor=db_conn.cursor()
-        mycursor.execute(sql,t_id)
-    
-        fields = map(lambda x:x[0], mycursor.description)
-        result = [dict(zip(fields,row)) for row in mycursor.fetchall()]
+def view_teacher_details(db_conn, teacher_id):
+    """[summary]
+
+    Args:
+        db_conn ([type]): [description]
+        teacher_id ([type]): [description]
+
+    Returns:
+        [type]: [description]
+    """    
+    if validate_teache_id(db_conn, teacher_id):
+        t_id = (teacher_id,)
+        sql = "select * from teachers where id=%s"
+
+        mycursor = db_conn.cursor()
+        mycursor.execute(sql, t_id)
+
+        fields = map(lambda x: x[0], mycursor.description)
+        result = [dict(zip(fields, row)) for row in mycursor.fetchall()]
         return result
     else:
         return None
 
-def update_teacher_record(db_conn,col_name,new_val,t_id):
-    sql="UPDATE teachers SET {}=%s WHERE id=%s".format(col_name)
-    val=(new_val,t_id,)
-    mycursor=db_conn.cursor()
-    mycursor.execute(sql,val)
+
+def update_teacher_record(db_conn, col_name, new_val, t_id):
+    """[summary]
+
+    Args:
+        db_conn ([type]): [description]
+        col_name ([type]): [description]
+        new_val ([type]): [description]
+        t_id ([type]): [description]
+    """    
+    sql = "UPDATE teachers SET {}=%s WHERE id=%s".format(col_name)
+    val = (new_val, t_id,)
+    mycursor = db_conn.cursor()
+    mycursor.execute(sql, val)
     db_conn.commit()
-    # mycursor.close()
-    print(mycursor.rowcount,"record updated")
+    print(mycursor.rowcount, "record updated")
 
 
-def delete_teacher(db_conn,teacher_id):
-    sql="DELETE FROM teachers WHERE id=%s"
-    val=(teacher_id,)
-    mycursor=db_conn.cursor()
-    mycursor.execute(sql,val)
+def delete_teacher(db_conn, teacher_id):
+    """[summary]
+
+    Args:
+        db_conn ([type]): [description]
+        teacher_id ([type]): [description]
+    """    
+    sql = "DELETE FROM teachers WHERE id=%s"
+    val = (teacher_id,)
+    mycursor = db_conn.cursor()
+    mycursor.execute(sql, val)
     db_conn.commit()
-    print(mycursor.rowcount,"record deleted")
+    print(mycursor.rowcount, "record deleted")
 
 
-def validate_teache_id(db_conn,teacher_id):
-    t_id=(teacher_id,)
-    sql="select id from teachers where id=%s"
-    mycursor=db_conn.cursor()
-    mycursor.execute(sql,t_id)
-    record=mycursor.fetchone()
-    row_count=mycursor.rowcount
-    if row_count==0:
+def validate_teache_id(db_conn, teacher_id):
+    """[summary]
+
+    Args:
+        db_conn ([type]): [description]
+        teacher_id ([type]): [description]
+
+    Returns:
+        [type]: [description]
+    """    
+    t_id = (teacher_id,)
+    sql = "select id from teachers where id=%s"
+    mycursor = db_conn.cursor()
+    mycursor.execute(sql, t_id)
+    record = mycursor.fetchone()
+    row_count = mycursor.rowcount
+    if row_count == 0:
         return False
     else:
         return True
 
+
 def get_column_names(db_conn):
-        sql="select * from teachers"
-        
-        mycursor=db_conn.cursor()
-        mycursor.execute(sql)
-        # record=mycursor.fetchone()
-    
-        col_names=list(map(lambda x:x[0], mycursor.description))
-        # mycursor.close()
-        # print(col_names)
-        mycursor.reset()
-        return col_names
+    """[summary]
 
+    Args:
+        db_conn ([type]): [description]
 
+    Returns:
+        [type]: [description]
+    """    
+    sql = "select * from teachers"
 
+    mycursor = db_conn.cursor()
+    mycursor.execute(sql)
+
+    col_names = list(map(lambda x: x[0], mycursor.description))
+    mycursor.reset()
+    return col_names
